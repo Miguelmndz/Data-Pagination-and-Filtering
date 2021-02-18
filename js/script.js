0d29511773b15b43740921deb43bc5a65ce59ba5
Miguel Mendez <michaelmndz1141@gmail.com>
2021-02-18 10:34:31 -0700

    created student data to display
    Added function to populate the screen with students

--- js/script.js
+++ js/script.js
@@ -4,20 +4,36 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage( list, page ) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentListUl = document.querySelector('.student-list');
+   studentListUl.innerHTML = '';
 
+   // if the index is greater or same as the starting indexc and less than the endindex
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         console.log(startIndex, endIndex);
+         const studentData = `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src=${list[i].picture.medium}></img>
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>`
+         // insert elements to the studentListUl
+         studentListUl.insertAdjacentHTML("beforeend", studentData);
+      }
+   }
+}
 
 
 /*
@@ -28,3 +44,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 2);
